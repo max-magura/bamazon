b5ee6785d7a6fe9405c3f39af14dd7a3e06d8b7e
Max-M <maxnmagura@gmail.com>
2019-10-13 18:34:40 -0500

    added inquirer questions and function to read to see if quantity was low or high

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,10 +14,60 @@ var connection = mysql.createConnection(connectionObject);
 
 connection.connect(function(err) {
   if(err) throw err;
-  console.log("connected as id " + connection.threadId)
-  afterConnection();
+  // console.log("connected as id " + connection.threadId)
+  displayProducts();
 });
 
-function afterConnection() {
-  console.log("It's alive!")
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, result) {
+    if (err) throw err;
+    console.table(result);
+    startInq();
+  });
+};
+
+function startInq() {
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+
+  inquirer.prompt([
+    {
+    name: "itemID",
+    type: "input",
+      message: "What is the item ID for the product you would like to buy?"
+    },
+    {
+    name: "quantity",
+    type: "input",
+    message: "How many units would you like to purchase?",
+    validate: function(value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return false;
+    }
+    }])
+    .then(function(answer) {
+
+      var chosenProduct;
+
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].item_id === parseInt(answer.itemID)) {
+          chosenProduct = results[i];
+        }
+      }
+
+      // console.log(chosenProduct)
+
+      if (chosenProduct.stock_quantity > parseInt(answer.quantity)){
+        console.log("We got plenty!!")
+      
+      }
+      else {
+        console.log("We ain't got enough.")
+      }
+
+    })
+
+  })
 };
